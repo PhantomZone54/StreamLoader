69be4b22920c4a61bf4755d7c376af1e57a4ac9a
Rokib Hasan Sagar <10854164+rokibhasansagar@users.noreply.github.com>
2022-02-05 17:53:47 +0600

    Update av1-encoder.sh
    - Improvements

--- av1-encoder.sh
+++ av1-encoder.sh
@@ -6,26 +6,31 @@ echo "::group:: [i] Download the File Chunk"
 export InputChunkFullName="${AnimeName}.Ep${Episode}.${AudioType}.part_${ChunkNum}.mkv" # ${ChunkNum} <<${{ matrix.ChunkNum }}
 rclone copy ${LocationOnIndex}/${AnimeName}/${ChunkDir}/${InputChunkFullName} . && printf "Downloaded Working Chunk\n"
 export R3ncod3dChunkName=$(echo ${InputChunkFullName} | sed 's|mkv|480p.av1.mkv|;s|.mkv||')
+export ScaleFlags="format=pix_fmts=yuv420p10le,scale=w=-2:h=480:out_color_matrix=bt709:out_range=tv:flags=lanczos"
+export AomParams="tune=ssim:enable-qm=1:arnr-maxframes=5:arnr-strength=2"
 echo "::endgroup::"
 
 mkdir -p ${ChunkEncDir}
 
-echo "::group:: [i] Pass the SubChunk through r3ncod3r - RCMode=Q"
-printf "Archival r3ncode - Pass 1 of 2\n"
+echo "::group:: [i] Archival r3ncode - Pass 1 of 2"
 ${FTOOL_CONVERTER} -loglevel warning -stats -stats_period 4 \
   -i ${InputChunkFullName} -map_metadata -1 -map_chapters -1 -map 0:V -c:v libaom-av1 \
-  -vf "format=pix_fmts=yuv420p10le,scale=-2:480" -g $((FrameRate * 4)) -keyint_min ${FrameRate} \
-  -usage good -cpu-used 2 -crf ${QScale} -b:v 0 -aq-mode 2 -tune ssim \
-  -lag-in-frames ${FrameRate} -tile-columns 2 -tile-rows 1 -frame-parallel true -row-mt true \
+  -vf ${ScaleFlags} -g $((FrameRate * 4)) -keyint_min ${FrameRate} \
+  -usage good -cpu-used 2 -crf ${QScale} -b:v 0 -lag-in-frames 48 -threads 4 \
+  -tile-columns 1 -tile-rows 0 -frame-parallel false -row-mt true \
+  -aq-mode 1 -aom-params ${AomParams} -qmin $((QScale/3)) -qmax $((QScale + QScale/3 + 2))
   -pass 1 -avoid_negative_ts 1 -f null /dev/null
-sleep 2s && echo && sleep 1s
+echo "::endgroup::"
 
-printf "Archival r3ncode - Pass 2 of 2\n"
+sleep 2s && echo && sleep 2s
+
+echo "::group:: [i] Archival r3ncode - Pass 1 of 2"
 ${FTOOL_CONVERTER} -loglevel warning -stats -stats_period 20 \
   -i ${InputChunkFullName} -map_metadata -1 -map_chapters -1 -map 0:V -c:v libaom-av1 \
-  -vf "format=pix_fmts=yuv420p10le,scale=-2:480" -g $((FrameRate * 4)) -keyint_min ${FrameRate} \
-  -usage good -cpu-used ${SpeedProf} -crf ${QScale} -b:v 0 -aq-mode 2 -tune ssim \
-  -lag-in-frames ${FrameRate} -tile-columns 2 -tile-rows 1 -frame-parallel true -row-mt true \
+  -vf ${ScaleFlags} -g $((FrameRate * 4)) -keyint_min ${FrameRate} \
+  -usage good -cpu-used ${SpeedProf} -crf ${QScale} -b:v 0 -lag-in-frames 48 -threads 4 \
+  -tile-columns 1 -tile-rows 0 -frame-parallel false -row-mt true \
+  -aq-mode 1 -aom-params ${AomParams} -qmin $((QScale/3)) -qmax $((QScale + QScale/3 + 2))
   -pass 2 -auto-alt-ref 1 -avoid_negative_ts 1 -f webm -dash 1 \
   ${ChunkEncDir}/${R3ncod3dChunkName}.webm 2>&1 | tee ${R3ncod3dChunkName}.log
 sed -i 's|\r|\n|g' "${R3ncod3dChunkName}.log"
