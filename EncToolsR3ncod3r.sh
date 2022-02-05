0efe2b01a9ade4b5c4c7228d8b93145b965c61b1
Rokib Hasan Sagar <10854164+rokibhasansagar@users.noreply.github.com>
2022-02-05 18:00:55 +0600

    Update EncToolsR3ncod3r.sh
    - FTool compression

--- EncToolsR3ncod3r.sh
+++ EncToolsR3ncod3r.sh
@@ -6,10 +6,13 @@ set -eo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 echo "::group:: Prepare R3ncod3r Tool"
-printf "Installing Required Applications for R3ncod3r...\n"
+printf "Adding Required Applications for R3ncod3r...\n"
 cd "$(mktemp -d)"
 wget -q "${FTOOL_ARC_URL}" || curl -sL "${FTOOL_ARC_URL}" -O
 tar -xJf ff*.tar.xz --strip-components 1
+printf "Compressing Binaries before Use\n"
+upx -q -1 --no-backup bin/*
+chmod a+x bin/*
 sudo mv bin/* /usr/local/bin/
 cd -
 ${FTOOL_CONVERTER} -version
